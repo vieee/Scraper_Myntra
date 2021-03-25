8727aa66b781cfca90917a4e8e4c300e3c86f56b
Deepak Yadav <dgyadav1782000@gmail.com>
2021-03-26 02:36:53 +0530

    Scrapy Myntra in Output

--- postscrape/postscrape/spiders/posts_spider.py
+++ postscrape/postscrape/spiders/posts_spider.py
@@ -7,6 +7,7 @@ from scrapy.selector import Selector
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 import re
 import time
 from pprint import pprint
